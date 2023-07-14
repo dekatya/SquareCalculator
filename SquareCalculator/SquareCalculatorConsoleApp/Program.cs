2e0c668677ce764e4bb607ad8a73a8c80f13bde1
edekhanova <esdekhanova@gmail.com>
2023-07-14 13:30:58 +0300

    Refactoring

--- SquareCalculator/SquareCalculatorConsoleApp/Program.cs
+++ SquareCalculator/SquareCalculatorConsoleApp/Program.cs
@@ -1,59 +1,16 @@
 ﻿using SquareCalculatorLibrary;
+using SquareCalculatorLibrary.Shapes;
 
-var factory = new ShapeFactory();
-var shape = String.Empty;
-var isCorrectShape = false;
-
-while (!isCorrectShape)
+var list = new List<ShapeAbstract>()
 {
-    Console.WriteLine("Выберите фигуру: круг (к) или треугольник (т)");
-    shape = Console.ReadLine();
-    isCorrectShape = (shape == "к" || shape == "т") ? true : false;
-}
+    new Triangle(3, 4, 5),
+    new Triangle(3, 6, 5),
+    new Circle(4),
+    new Circle(5)
+};
 
-if (shape == "к")
+var squareCalculator = new SquareCalculator();
+foreach (var shape in list)
 {
-    Console.WriteLine("Укажите радиус круга");
-    var radius = Console.ReadLine();
-    if (Double.TryParse(radius, out var result))
-    {
-        var circle = factory.CreateCircle(result);
-        if (!circle.IsRadiusValid())
-            Console.WriteLine("Радиус круга невалиден");
-        else
-        {
-            Console.WriteLine($"Площадь круга = {circle.CalculateSquare()}");
-        }
-    }
-    else
-    {
-        Console.WriteLine("Радиус круга должен быть десятичным числом");
-    }
-}
-else
-{
-    Console.WriteLine("Укажите стороны треугольника.\nСторона а:");
-    var sideA = Console.ReadLine();
-    Console.WriteLine("Сторона b:");
-    var sideB = Console.ReadLine();
-    Console.WriteLine("Сторона c:");
-    var sideC = Console.ReadLine();
-
-    if (Double.TryParse(sideA, out var resultA)
-        && Double.TryParse(sideB, out var resultB)
-        && Double.TryParse(sideC, out var resultC))
-    {
-        var triangle = factory.CreateTriangle(resultA, resultB, resultC);
-        if (!triangle.AreTriangleSidesValid())
-            Console.WriteLine("Стороны треугольника невалидны");
-        else
-        {
-            Console.WriteLine($"Площадь треугольника = {triangle.CalculateSquare()}");
-            Console.WriteLine($"Прямоугольный ли треугольник = {triangle.IsRightAngled()}");
-        }
-    }
-    else
-    {
-        Console.WriteLine("Стороны треугольника должны быть десятичными числами");
-    }
+    Console.WriteLine(squareCalculator.Calculate(shape));
 }
---
+++ SquareCalculator/SquareCalculatorLibrary/SquareCalculator.cs
@@ -0,0 +1,14 @@
+﻿using SquareCalculatorLibrary.Shapes;
+
+namespace SquareCalculatorLibrary
+{
+    public class SquareCalculator
+    {
+        public SquareCalculator() { }
+
+        public double Calculate(ShapeAbstract shape)
+        {
+            return shape.CalculateSquare();
+        }
+    }
+}
